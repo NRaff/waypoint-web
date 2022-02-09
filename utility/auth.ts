aec21601c11582d1d77fe548142946218dd35580
nraff <nraff510@gmail.com>
2022-02-09 12:42:59 -0800

    update auth to a dynamic page

--- utility/auth.ts
+++ utility/auth.ts
@@ -2,13 +2,15 @@ import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth'
 
 const auth = getAuth();
 
+// TODO: consider changing options passed in to a payload 
+// TODO: (simplifies crossover between login and signup actions)
 export function createUserEP(email: string, password: string) {
   createUserWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
       // Signed in 
       const user = userCredential.user;
-      console.log(user)
-      // ...
+      // TODO: use user.updateProfile to set display name and other details
+        // TODO: would be good to set a default profile image
     })
     .catch((error) => {
       const errorCode = error.code;
