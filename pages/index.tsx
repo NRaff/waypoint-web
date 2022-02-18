73f4b2b42f238da15f369b7322a989c84325ddcf
nraff <nraff510@gmail.com>
2022-02-18 10:17:24 -0800

    update nav css

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,12 @@
 import styles from '@/styles/modules/index.module.css'
-import { useSelector, useDispatch } from 'react-redux'
-import { updateBase } from 'redux/actions/actions'
 import Link from 'next/link'
 import Landing from '@/components/Landing'
-
 
 export default function Home() {
   return (
     <Landing home>
       <nav className={styles.landing}>
-        <h1>Waypoint</h1>
+        <h1 className={styles.waypoint}>⛵️ Waypoint</h1>
         <menu className={styles.authOptions}>
           <Link href='/auth/login'>
             <a>Login</a>
