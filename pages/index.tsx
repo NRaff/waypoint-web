596774af5eb2bf94baaeaeb7451ab1f1b798f7d7
nraff <nraff510@gmail.com>
2022-02-18 10:12:08 -0800

    add nav bar to landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,22 +6,19 @@ import Landing from '@/components/Landing'
 
 
 export default function Home() {
-  const dispatch = useDispatch()
-  const currentCount = useSelector((state: any) => state.testReducer)
   return (
     <Landing home>
-      <header>
-        <h1>Waypoint Web: {currentCount}</h1>
-        {/* <button
-          onClick={() => dispatch(updateBase())}
-        >Add One</button> */}
-        <Link href='/auth/login'>
-          <a>Login</a>
-        </Link>
-        <Link href='/auth/signup'>
-          <a>Signup</a>
-        </Link>
-      </header>
+      <nav className={styles.landing}>
+        <h1>Waypoint</h1>
+        <menu className={styles.authOptions}>
+          <Link href='/auth/login'>
+            <a>Login</a>
+          </Link>
+          <Link href='/auth/signup'>
+            <a>Signup</a>
+          </Link>
+        </menu>
+      </nav>
     </Landing>
   )
 }
