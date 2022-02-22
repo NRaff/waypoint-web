8099a3a26b436ba60fef16042bcf05abc65893b5
nraff <nraff510@gmail.com>
2022-02-22 11:55:51 -0800

    better sign in css

--- components/Auth.tsx
+++ components/Auth.tsx
@@ -39,31 +39,38 @@ export default function Auth({authType} : any) {
 
   return (
     <section className={styles.authContainer}>
-      <label className={styles.authLabel}>Email</label>
-      <input 
-        type="text"
-        value={email}
-        onChange={e => setEmail(e.target.value)}
-        className={styles.authInput}
-      />
-      {showSignUp()}
-      <label className={styles.authLabel}>Password</label>
-      <input 
-        type="password"
-        value={password}
-        onChange={e => setPassword(e.target.value)}
-        className={styles.authInput}
-      />
-      <button
-        onClick={() => createUserEP(payload(), dispatch)}
-        className={styles.authButton}
-      >Login</button>
-      <BtnWithImg
-        action={() => signupWithGoogle(dispatch)}
-        img='/assets/btn_google_light_normal_ios.svg'
-        title='Sign in with Google'
-        style='googleSignIn'
-      />
+      <section className={styles.withEmail}>
+        <label className={styles.authLabel}>Email</label>
+        <input
+          type="text"
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+          className={styles.authInput}
+          placeholder='waypoint@email.com'
+        />
+        {showSignUp()}
+        <label className={styles.authLabel}>Password</label>
+        <input
+          type="password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+          className={styles.authInput}
+          placeholder='password'
+        />
+        <button
+          onClick={() => createUserEP(payload(), dispatch)}
+          className={styles.authButton}
+        >Login</button>
+      </section>
+      <hr className={styles.authDivider}></hr>
+      <section className={styles.withServices}>
+        <BtnWithImg
+          action={() => signupWithGoogle(dispatch)}
+          img='/assets/btn_google_light_normal_ios.svg'
+          title='Sign in with Google'
+          style='googleSignIn'
+        />
+      </section>
     </section>
   )
 }
